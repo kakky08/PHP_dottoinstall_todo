0f707470663d1df49b410a9e1ab444ce50aab9d0
kakky08 <efforts.pay.off.k92uk1@gmail.com>
2021-10-12 20:41:38 +0900

    todo class

--- work/app/Todo.php
+++ work/app/Todo.php
@@ -30,6 +30,9 @@ class Todo
                 case 'delete':
                     $this->delete();
                     break;
+                case 'purge':
+                    $this->purge();
+                    break;
                 default:
                     exit;
             }
@@ -77,6 +80,10 @@ class Todo
         $stmt->execute();
     }
 
+    private function purge()
+    {
+        $this->pdo->query("DELETE FROM todos WHERE is_done = 1");
+    }
 
     public function getAll()
     {
